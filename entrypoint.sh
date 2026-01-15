67018bba7b9bb55e40d30b4af61d6633b1ee98a2
franciscolaria-commits <francisco.laria.1234@gmail.com>
2026-01-15 10:19:49 -0300

    Fix: Dockerfile directo y borrado entrypoint

--- entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Crear las tablas de la base de datos (por si no existen)
-python -c "from app import create_app, db; app = create_app(); app.app_context().push(); db.create_all()"
-
-# Iniciar la app usando Gunicorn (Servidor pro)
-exec gunicorn --bind 0.0.0.0:10000 run:app
